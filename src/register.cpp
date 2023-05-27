91f48b8ff830f7263eff70895b318603b56285f4
doaads <nowobart@gmail.com>
2023-05-27 18:09:53 +0200

    replaced for_each with standard for loop in register.cpp

--- src/register.cpp
+++ src/register.cpp
@@ -125,23 +125,23 @@ std::string Register::workersString() {
 
 std::vector<std::string> Register::getWorkerNamesByType(employeeType type) {
 	std::vector<std::string> resultVec;
-	std::for_each(workers.begin(), workers.end(), 
-			[type](std::unique_ptr<Worker>& worker, auto& resultVec) {
-				if (worker -> getType() == type) {
-					resultVec.push_back(worker -> getName());
-				}
-			});
+	for (auto& worker : workers) {
+		if (worker -> getType() == type) {
+			resultVec.push_back(worker -> getName());
+		}
+
+	}
 	return resultVec;
 }
 
 std::vector<std::string> Register::getWorkerByTypeAndAvailability(employeeType type, Weekday day) {
 	std::vector<std::string> resultVec;
-	std::for_each(workers.begin(), workers.end(), 
-			[type, day](std::unique_ptr<Worker>& worker, auto& resultVec) {
-				if ((worker -> getType() == type) && (worker -> isAvailable(day))){
-					resultVec.push_back(worker -> getName());
-				}
-			});
+	for (auto& worker : workers) {
+		if (worker -> getType() == type && worker -> isAvailable(day)) {
+			resultVec.push_back(worker -> getName());
+		}
+
+	}
 	return resultVec;
 }
 
