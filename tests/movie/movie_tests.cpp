dbf6059ad863394ad64c579c94bc1537d0ece161
Michał Pałasz <01178574@pw.edu.pl>
2023-05-30 11:49:01 +0200

    Added few tests

---
+++ tests/movie/movie_tests.cpp
@@ -0,0 +1,31 @@
+#include "catch_amalgamated.hpp"
+#include <iostream>
+#include <sstream>
+#include "../../src/movie.h"
+
+TEST_CASE("movie simple tests", "[movie]")
+{
+	Movie movie("Interstellar", 22, 156);
+
+	CHECK(movie.getName() == "Interstellar");
+	CHECK(movie.getPrice() == 22);
+	CHECK(movie.getDuration() == 156);
+
+	SECTION("testing setters")
+	{
+		movie.setName("Forrest Gump");
+		movie.setPrice(15);
+		movie.setDuration(140);
+
+		CHECK(movie.getName() == "Forrest Gump");
+		CHECK(movie.getPrice() == 15);
+		CHECK(movie.getDuration() == 140);
+	}
+
+	SECTION("testing operator")
+	{
+		std::ostringstream oss;
+		oss << movie;
+		CHECK(oss.str() == "Movies name: Interstellar\nMovies price: 22\nMovies length: 156 minutes\n");
+	}
+}
