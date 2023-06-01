e0040c16a48f6668e4862c6d4e26ffb7aa41f5d7
Bartosz Nowak <nowobart@gmail.com>
2023-06-01 12:16:48 +0200

    ddd

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
 
 int main(int argc, char *argv[]) 
 {
-	if(argc != 4)
+	if(argc != 7)
     {
-        std::cerr << "Incorrect number of command line arguments - expected 2, got " << argc << std::endl;
+        std::cerr << "Incorrect number of command line arguments - expected 6, got " << argc - 1<< std::endl;
         return 1;
     }
 
@@ -15,6 +15,8 @@ int main(int argc, char *argv[])
 	std::string movieData;
 	std::string employeeData;
 	std::string counterData;
+	unsigned opening;
+	unsigned closing;
 
 	std::stringstream stream;
     stream << argv[1];
@@ -27,23 +29,38 @@ int main(int argc, char *argv[])
 
 	stream << argv[3];
 	stream >> employeeData;
+	stream.clear();
 
 	stream << argv[4];
 	stream >> counterData;
+	stream.clear();
+
+	stream << argv[5];
+	stream >> opening;
+	stream.clear();
+
+	stream << argv[6];
+	stream >> closing;
+	stream.clear();
 
 
 	//Wywolanie symulacji z dodatkowymi zmiennymi, czyli sciezkami do odpowiednich plikow
-	Simulation sim("kino", 10, 20);
+	Simulation sim("kino", opening, closing);
 	sim.setRoomsPath(cinemaData);
 	sim.setEmployeePath(employeeData);
 	sim.setMoviePath(movieData);
 	sim.setWorkplacePath(counterData);
 	sim.startSimulation();
 	std::string answer = "";
-	while (true) {
+
+	bool running = true;
+	while (running) {
 		std::cout << sim.runStep() << std::endl;
 		std::cin >> answer;
-		sim.getCurrentStep();
+		if (answer == "exit") {
+			running = false;
+		}
+
 
 	}
 
